d5578b5014719d002c7f32e4fcbc571f4bc90ee3
HelmutM87 <127511280+HelmutM87@users.noreply.github.com>
2024-12-16 17:31:59 -0300

    Added navbar details

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,10 +1,211 @@
 
+// import { ApplicationConfig } from '@angular/core';
+// import { provideRouter } from '@angular/router';
+// import { provideClientHydration } from '@angular/platform-browser';
+// import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
+// import { provideHttpClient } from '@angular/common/http';
+// import { TranslateLoader } from '@ngx-translate/core';
+// import { TranslateHttpLoader } from '@ngx-translate/http-loader';
+// import { HttpClient } from '@angular/common/http';
+// import { routes } from './app.routes';
+
+// // Funktion, um den HttpLoader für die Übersetzungsdateien bereitzustellen
+// export function HttpLoaderFactory(http: HttpClient) {
+//   return new TranslateHttpLoader(http, './assets/i18n/', '.json');
+// }
+
+// export const appConfig: ApplicationConfig = {
+//   providers: [
+//     provideRouter(routes), // Routes einbinden
+//     provideClientHydration(),
+//     provideAnimationsAsync(),
+//     provideHttpClient(),
+//     {
+//       provide: TranslateLoader,
+//       useFactory: HttpLoaderFactory,
+//       deps: [HttpClient]
+//     }, provideAnimationsAsync()
+//   ]
+// };
+
+
+// import { ApplicationConfig } from '@angular/core';
+// import { provideRouter } from '@angular/router';
+// import { provideClientHydration } from '@angular/platform-browser';
+// import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
+// import { provideHttpClient } from '@angular/common/http';
+// import { provideTranslations, TranslateLoader } from '@ngx-translate/core';
+// import { TranslateHttpLoader } from '@ngx-translate/http-loader';
+// import { HttpClient } from '@angular/common/http';
+// import { routes } from './app.routes';
+
+// // Funktion, um den HttpLoader für die Übersetzungsdateien bereitzustellen
+// export function HttpLoaderFactory(http: HttpClient) {
+//   return new TranslateHttpLoader(http, './assets/i18n/', '.json');
+// }
+
+// export const appConfig: ApplicationConfig = {
+//   providers: [
+//     provideRouter(routes), // Routes einbinden
+//     provideClientHydration(),
+//     provideAnimationsAsync(),
+//     provideHttpClient(),
+//     provideTranslations({
+//       loader: {
+//         provide: TranslateLoader,
+//         useFactory: HttpLoaderFactory,
+//         deps: [HttpClient]
+//       }
+//     })
+//   ]
+// };
+
+
+// import { ApplicationConfig } from '@angular/core';
+// import { provideRouter } from '@angular/router';
+// import { provideClientHydration } from '@angular/platform-browser';
+// import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
+// import { provideHttpClient } from '@angular/common/http';
+// import { TranslateModule, TranslateLoader } from '@ngx-translate/core';
+// import { TranslateHttpLoader } from '@ngx-translate/http-loader';
+// import { HttpClient } from '@angular/common/http';
+// import { routes } from './app.routes';
+
+// // Funktion, um den HttpLoader für die Übersetzungsdateien bereitzustellen
+// export function HttpLoaderFactory(http: HttpClient) {
+//   return new TranslateHttpLoader(http, './assets/i18n/', '.json');
+// }
+
+// export const appConfig: ApplicationConfig = {
+//   providers: [
+//     provideRouter(routes), // Routes einbinden
+//     provideClientHydration(),
+//     provideAnimationsAsync(),
+//     provideHttpClient(),
+//     {
+//       provide: TranslateLoader,
+//       useFactory: HttpLoaderFactory,
+//       deps: [HttpClient]
+//     },
+//     ...TranslateModule.forRoot({
+//       loader: {
+//         provide: TranslateLoader,
+//         useFactory: HttpLoaderFactory,
+//         deps: [HttpClient]
+//       }
+//     }).providers
+//   ]
+// };
+
+
+
+// import { ApplicationConfig } from '@angular/core';
+// import { provideRouter } from '@angular/router';
+// import { provideClientHydration } from '@angular/platform-browser';
+// import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
+// import { provideHttpClient } from '@angular/common/http';
+// import { TranslateModule, TranslateLoader } from '@ngx-translate/core';
+// import { TranslateHttpLoader } from '@ngx-translate/http-loader';
+// import { HttpClient } from '@angular/common/http';
+// import { routes } from './app.routes';
+
+// // Funktion, um den HttpLoader für die Übersetzungsdateien bereitzustellen
+// export function HttpLoaderFactory(http: HttpClient) {
+//   return new TranslateHttpLoader(http, './assets/i18n/', '.json');
+// }
+
+// export const appConfig: ApplicationConfig = {
+//   providers: [
+//     provideRouter(routes), // Routes einbinden
+//     provideClientHydration(),
+//     provideAnimationsAsync(),
+//     provideHttpClient(),
+//     {
+//       provide: TranslateLoader,
+//       useFactory: HttpLoaderFactory,
+//       deps: [HttpClient]
+//     },
+//     ...(TranslateModule.forRoot({
+//       loader: {
+//         provide: TranslateLoader,
+//         useFactory: HttpLoaderFactory,
+//         deps: [HttpClient]
+//       }
+//     }).providers ?? [])
+//   ]
+// };
+
+
+
+// import { ApplicationConfig } from '@angular/core';
+// import { provideRouter } from '@angular/router';
+// import { provideClientHydration } from '@angular/platform-browser';
+// import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
+// import { provideHttpClient } from '@angular/common/http';
+// import { TranslateModule, TranslateLoader } from '@ngx-translate/core';
+// import { TranslateHttpLoader } from '@ngx-translate/http-loader';
+// import { HttpClient } from '@angular/common/http';
+// import { routes } from './app.routes';
+
+// // Funktion, um den HttpLoader für die Übersetzungsdateien bereitzustellen
+// export function HttpLoaderFactory(http: HttpClient) {
+//   return new TranslateHttpLoader(http, './assets/i18n/', '.json');
+// }
+
+// export const appConfig: ApplicationConfig = {
+//   providers: [
+//     provideRouter(routes), // Routes einbinden
+//     provideClientHydration(),
+//     provideAnimationsAsync(),
+//     provideHttpClient(),
+//     ...TranslateModule.forRoot({
+//       loader: {
+//         provide: TranslateLoader,
+//         useFactory: HttpLoaderFactory,
+//         deps: [HttpClient]
+//       }
+//     }).providers
+//   ]
+// };
+
+// import { ApplicationConfig } from '@angular/core';
+// import { provideRouter } from '@angular/router';
+// import { provideClientHydration } from '@angular/platform-browser';
+// import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
+// import { provideHttpClient } from '@angular/common/http';
+// import { TranslateModule, TranslateLoader } from '@ngx-translate/core';
+// import { TranslateHttpLoader } from '@ngx-translate/http-loader';
+// import { HttpClient } from '@angular/common/http';
+// import { routes } from './app.routes';
+
+// // Funktion, um den HttpLoader für die Übersetzungsdateien bereitzustellen
+// export function HttpLoaderFactory(http: HttpClient) {
+//   return new TranslateHttpLoader(http, './assets/i18n/', '.json');
+// }
+
+// export const appConfig: ApplicationConfig = {
+//   providers: [
+//     provideRouter(routes), // Routes einbinden
+//     provideClientHydration(),
+//     provideAnimationsAsync(),
+//     provideHttpClient(),
+//     ...(TranslateModule.forRoot({
+//       loader: {
+//         provide: TranslateLoader,
+//         useFactory: HttpLoaderFactory,
+//         deps: [HttpClient]
+//       }
+//     }).providers ?? [])
+//   ]
+// };
+
+
 import { ApplicationConfig } from '@angular/core';
 import { provideRouter } from '@angular/router';
 import { provideClientHydration } from '@angular/platform-browser';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 import { provideHttpClient } from '@angular/common/http';
-import { TranslateLoader } from '@ngx-translate/core';
+import { TranslateModule, TranslateLoader, TranslateService } from '@ngx-translate/core';
 import { TranslateHttpLoader } from '@ngx-translate/http-loader';
 import { HttpClient } from '@angular/common/http';
 import { routes } from './app.routes';
@@ -20,10 +221,13 @@ export const appConfig: ApplicationConfig = {
     provideClientHydration(),
     provideAnimationsAsync(),
     provideHttpClient(),
-    {
-      provide: TranslateLoader,
-      useFactory: HttpLoaderFactory,
-      deps: [HttpClient]
-    }
+    TranslateService, // Hinzufügen von TranslateService
+    ...(TranslateModule.forRoot({
+      loader: {
+        provide: TranslateLoader,
+        useFactory: HttpLoaderFactory,
+        deps: [HttpClient]
+      }
+    }).providers ?? [])
   ]
 };
--- src/app/shared/navbar/navbar.component.ts
+++ src/app/shared/navbar/navbar.component.ts
@@ -1,15 +1,22 @@
-import { Component } from '@angular/core';
+// import { Component } from '@angular/core';
+// import {MatIconModule} from '@angular/material/icon';
+// import {MatMenuModule} from '@angular/material/menu';
+// import { TranslateModule, TranslateService } from '@ngx-translate/core';
 
-@Component({
-  selector: 'app-navbar',
-  standalone: true,
-  imports: [],
-  templateUrl: './navbar.component.html',
-  styleUrl: './navbar.component.scss'
-})
-export class NavbarComponent {
+// @Component({
+//   selector: 'app-navbar',
+//   standalone: true,
+//   imports: [
+//     MatIconModule,
+//     MatMenuModule,
+//     TranslateService,
+//   ],
+//   templateUrl: './navbar.component.html',
+//   styleUrl: './navbar.component.scss'
+// })
+// export class NavbarComponent {
 
-}
+// }
 
 
 // import { Component } from '@angular/core';
@@ -62,3 +69,190 @@ export class NavbarComponent {
 //   }
 // }
 
+
+
+// import { Component } from '@angular/core';
+// import { MatIconModule } from '@angular/material/icon';
+// import { MatMenuModule } from '@angular/material/menu';
+// import { TranslateModule, TranslateService } from '@ngx-translate/core';
+// import { MatDialog, MatDialogModule } from '@angular/material/dialog';
+
+// @Component({
+//   selector: 'app-navbar',
+//   standalone: true,
+//   imports: [
+//     MatIconModule,
+//     MatMenuModule,
+//     TranslateModule,
+//     MatDialogModule
+//   ],
+//   providers: [TranslateService], // Hier füge den Service hinzu
+//   templateUrl: './navbar.component.html',
+//   styleUrl: './navbar.component.scss'
+// })
+// export class NavbarComponent {
+//   currentLang = 'de'; // Beispielwert, du musst den aktuellen Sprachwert setzen
+
+//   // constructor(private translate: TranslateService) {
+//   //   // Hier könntest du die Sprache initialisieren, falls nötig
+//   // }
+
+//   constructor(private dialog: MatDialog, private translate: TranslateService) {
+//     this.currentLang = this.translate.currentLang || 'de'; // Standardsprache
+//   }
+
+//   switchLanguage(lang: string) {
+//     this.translate.use(lang);
+//     this.currentLang = lang;
+//   }
+
+//   openDialogPrice() {
+//     // Implementierung für Preisdialog
+//   }
+
+//   openDialogCalendar() {
+//     // Implementierung für Kalenderdialog
+//   }
+// }
+
+
+
+// import { Component, importProvidersFrom } from '@angular/core';
+// import { MatIconModule } from '@angular/material/icon';
+// import { MatMenuModule } from '@angular/material/menu';
+// import { TranslateModule, TranslateService, TranslateLoader, TranslateStore } from '@ngx-translate/core';
+// import { HttpClient, HttpClientModule } from '@angular/common/http';
+// import { TranslateHttpLoader } from '@ngx-translate/http-loader';
+// import { MatDialog, MatDialogModule } from '@angular/material/dialog';
+
+// // Aot compilation support
+// export function HttpLoaderFactory(http: HttpClient): TranslateHttpLoader {
+//   return new TranslateHttpLoader(http, './assets/i18n/', '.json');
+// }
+
+// @Component({
+//   selector: 'app-navbar',
+//   standalone: true,
+//   imports: [
+//     MatIconModule,
+//     MatMenuModule,
+//     MatDialogModule,
+//     HttpClientModule,
+//     TranslateModule.forRoot({
+//       loader: {
+//         provide: TranslateLoader,
+//         useFactory: HttpLoaderFactory,
+//         deps: [HttpClient]
+//       }
+//     })
+//   ],
+//   providers: [
+//     TranslateService,
+//     TranslateStore, // Dies stellt sicher, dass TranslateStore bereitgestellt wird
+//     { provide: TranslateLoader, useFactory: HttpLoaderFactory, deps: [HttpClient] }
+//   ],
+//   templateUrl: './navbar.component.html',
+//   styleUrl: './navbar.component.scss'
+// })
+// export class NavbarComponent {
+//   currentLang = 'de'; // Beispielwert, du musst den aktuellen Sprachwert setzen
+
+//   constructor(private dialog: MatDialog, private translate: TranslateService) {
+//     this.currentLang = this.translate.currentLang || 'de'; // Standardsprache
+//   }
+
+//   switchLanguage(lang: string) {
+//     this.translate.use(lang);
+//     this.currentLang = lang;
+//   }
+
+//   openDialogPrice() {
+//     // Implementierung für Preisdialog
+//   }
+
+//   openDialogCalendar() {
+//     // Implementierung für Kalenderdialog
+//   }
+// }
+
+
+
+// import { Component } from '@angular/core';
+// import { MatIconModule } from '@angular/material/icon';
+// import { MatMenuModule } from '@angular/material/menu';
+// import { TranslateModule } from '@ngx-translate/core';
+// import { MatDialog, MatDialogModule } from '@angular/material/dialog';
+
+// @Component({
+//   selector: 'app-navbar',
+//   standalone: true,
+//   imports: [
+//     MatIconModule,
+//     MatMenuModule,
+//     MatDialogModule,
+//     TranslateModule // Nur das Modul, ohne forRoot()
+//   ],
+//   templateUrl: './navbar.component.html',
+//   styleUrl: './navbar.component.scss'
+// })
+// export class NavbarComponent {
+//   currentLang = 'de'; // Beispielwert, du musst den aktuellen Sprachwert setzen
+
+//   constructor(private dialog: MatDialog, private translate: TranslateService) {
+//     this.currentLang = this.translate.currentLang || 'de'; // Standardsprache
+//   }
+
+//   switchLanguage(lang: string) {
+//     this.translate.use(lang);
+//     this.currentLang = lang;
+//   }
+
+//   openDialogPrice() {
+//     // Implementierung für Preisdialog
+//   }
+
+//   openDialogCalendar() {
+//     // Implementierung für Kalenderdialog
+//   }
+// }
+
+
+import { Component } from '@angular/core';
+import { MatIconModule } from '@angular/material/icon';
+import { MatMenuModule } from '@angular/material/menu';
+import { TranslateModule, TranslateService } from '@ngx-translate/core';
+import { MatDialog, MatDialogModule } from '@angular/material/dialog';
+
+@Component({
+  selector: 'app-navbar',
+  standalone: true,
+  imports: [
+    MatIconModule,
+    MatMenuModule,
+    MatDialogModule,
+    TranslateModule, // Nur das Modul, ohne forRoot()
+    
+  ],
+  templateUrl: './navbar.component.html',
+  styleUrl: './navbar.component.scss'
+})
+export class NavbarComponent {
+  currentLang = 'de'; // Beispielwert, du musst den aktuellen Sprachwert setzen
+
+  constructor(private dialog: MatDialog, private translate: TranslateService) {
+    this.currentLang = this.translate.currentLang || 'de'; // Standardsprache
+  }
+
+  switchLanguage(lang: string) {
+    this.translate.use(lang);
+    this.currentLang = lang;
+  }
+
+  openDialogPrice() {
+    // Implementierung für Preisdialog
+  }
+
+  openDialogCalendar() {
+    // Implementierung für Kalenderdialog
+  }
+}
