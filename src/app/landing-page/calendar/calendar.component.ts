dea1cf24fbca1a63f124f6063c6bd9a5a380bb9a
HelmutM87 <127511280+HelmutM87@users.noreply.github.com>
2024-12-17 17:06:54 -0300

    Revert "added calendar"
    This reverts commit c835664c275490bc3027cf4edd04653ff0d5d3ef.

--- src/app/landing-page/calendar/calendar.component.ts
+++ src/app/landing-page/calendar/calendar.component.ts
@@ -1,25 +1,9 @@
-import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
-import { FormsModule } from '@angular/forms';
-import { MatFormFieldModule } from '@angular/material/form-field';
-import {MatInputModule} from '@angular/material/input';
-import {MatButtonModule} from '@angular/material/button';
-import {MatDatepickerModule} from '@angular/material/datepicker';
-import { MatIconModule } from '@angular/material/icon';
-
 
 @Component({
   selector: 'app-calendar',
   standalone: true,
-  imports: [
-    CommonModule,
-    FormsModule,
-    MatFormFieldModule,
-    MatInputModule,
-    MatButtonModule,
-    MatDatepickerModule,
-    MatIconModule
-  ],
+  imports: [],
   templateUrl: './calendar.component.html',
   styleUrl: './calendar.component.scss'
 })
