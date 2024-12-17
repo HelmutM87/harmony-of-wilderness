355aab03d84d9de0ac9728455ed645a7c90a1adb
HelmutM87 <127511280+HelmutM87@users.noreply.github.com>
2024-12-17 18:18:45 -0300

    added translate initializer and set german language as default

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,206 +1,5 @@
 
-// import { ApplicationConfig } from '@angular/core';
-// import { provideRouter } from '@angular/router';
-// import { provideClientHydration } from '@angular/platform-browser';
-// import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
-// import { provideHttpClient } from '@angular/common/http';
-// import { TranslateLoader } from '@ngx-translate/core';
-// import { TranslateHttpLoader } from '@ngx-translate/http-loader';
-// import { HttpClient } from '@angular/common/http';
-// import { routes } from './app.routes';
-
-// // Funktion, um den HttpLoader für die Übersetzungsdateien bereitzustellen
-// export function HttpLoaderFactory(http: HttpClient) {
-//   return new TranslateHttpLoader(http, './assets/i18n/', '.json');
-// }
-
-// export const appConfig: ApplicationConfig = {
-//   providers: [
-//     provideRouter(routes), // Routes einbinden
-//     provideClientHydration(),
-//     provideAnimationsAsync(),
-//     provideHttpClient(),
-//     {
-//       provide: TranslateLoader,
-//       useFactory: HttpLoaderFactory,
-//       deps: [HttpClient]
-//     }, provideAnimationsAsync()
-//   ]
-// };
-
-
-// import { ApplicationConfig } from '@angular/core';
-// import { provideRouter } from '@angular/router';
-// import { provideClientHydration } from '@angular/platform-browser';
-// import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
-// import { provideHttpClient } from '@angular/common/http';
-// import { provideTranslations, TranslateLoader } from '@ngx-translate/core';
-// import { TranslateHttpLoader } from '@ngx-translate/http-loader';
-// import { HttpClient } from '@angular/common/http';
-// import { routes } from './app.routes';
-
-// // Funktion, um den HttpLoader für die Übersetzungsdateien bereitzustellen
-// export function HttpLoaderFactory(http: HttpClient) {
-//   return new TranslateHttpLoader(http, './assets/i18n/', '.json');
-// }
-
-// export const appConfig: ApplicationConfig = {
-//   providers: [
-//     provideRouter(routes), // Routes einbinden
-//     provideClientHydration(),
-//     provideAnimationsAsync(),
-//     provideHttpClient(),
-//     provideTranslations({
-//       loader: {
-//         provide: TranslateLoader,
-//         useFactory: HttpLoaderFactory,
-//         deps: [HttpClient]
-//       }
-//     })
-//   ]
-// };
-
-
-// import { ApplicationConfig } from '@angular/core';
-// import { provideRouter } from '@angular/router';
-// import { provideClientHydration } from '@angular/platform-browser';
-// import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
-// import { provideHttpClient } from '@angular/common/http';
-// import { TranslateModule, TranslateLoader } from '@ngx-translate/core';
-// import { TranslateHttpLoader } from '@ngx-translate/http-loader';
-// import { HttpClient } from '@angular/common/http';
-// import { routes } from './app.routes';
-
-// // Funktion, um den HttpLoader für die Übersetzungsdateien bereitzustellen
-// export function HttpLoaderFactory(http: HttpClient) {
-//   return new TranslateHttpLoader(http, './assets/i18n/', '.json');
-// }
-
-// export const appConfig: ApplicationConfig = {
-//   providers: [
-//     provideRouter(routes), // Routes einbinden
-//     provideClientHydration(),
-//     provideAnimationsAsync(),
-//     provideHttpClient(),
-//     {
-//       provide: TranslateLoader,
-//       useFactory: HttpLoaderFactory,
-//       deps: [HttpClient]
-//     },
-//     ...TranslateModule.forRoot({
-//       loader: {
-//         provide: TranslateLoader,
-//         useFactory: HttpLoaderFactory,
-//         deps: [HttpClient]
-//       }
-//     }).providers
-//   ]
-// };
-
-
-
-// import { ApplicationConfig } from '@angular/core';
-// import { provideRouter } from '@angular/router';
-// import { provideClientHydration } from '@angular/platform-browser';
-// import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
-// import { provideHttpClient } from '@angular/common/http';
-// import { TranslateModule, TranslateLoader } from '@ngx-translate/core';
-// import { TranslateHttpLoader } from '@ngx-translate/http-loader';
-// import { HttpClient } from '@angular/common/http';
-// import { routes } from './app.routes';
-
-// // Funktion, um den HttpLoader für die Übersetzungsdateien bereitzustellen
-// export function HttpLoaderFactory(http: HttpClient) {
-//   return new TranslateHttpLoader(http, './assets/i18n/', '.json');
-// }
-
-// export const appConfig: ApplicationConfig = {
-//   providers: [
-//     provideRouter(routes), // Routes einbinden
-//     provideClientHydration(),
-//     provideAnimationsAsync(),
-//     provideHttpClient(),
-//     {
-//       provide: TranslateLoader,
-//       useFactory: HttpLoaderFactory,
-//       deps: [HttpClient]
-//     },
-//     ...(TranslateModule.forRoot({
-//       loader: {
-//         provide: TranslateLoader,
-//         useFactory: HttpLoaderFactory,
-//         deps: [HttpClient]
-//       }
-//     }).providers ?? [])
-//   ]
-// };
-
-
-
-// import { ApplicationConfig } from '@angular/core';
-// import { provideRouter } from '@angular/router';
-// import { provideClientHydration } from '@angular/platform-browser';
-// import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
-// import { provideHttpClient } from '@angular/common/http';
-// import { TranslateModule, TranslateLoader } from '@ngx-translate/core';
-// import { TranslateHttpLoader } from '@ngx-translate/http-loader';
-// import { HttpClient } from '@angular/common/http';
-// import { routes } from './app.routes';
-
-// // Funktion, um den HttpLoader für die Übersetzungsdateien bereitzustellen
-// export function HttpLoaderFactory(http: HttpClient) {
-//   return new TranslateHttpLoader(http, './assets/i18n/', '.json');
-// }
-
-// export const appConfig: ApplicationConfig = {
-//   providers: [
-//     provideRouter(routes), // Routes einbinden
-//     provideClientHydration(),
-//     provideAnimationsAsync(),
-//     provideHttpClient(),
-//     ...TranslateModule.forRoot({
-//       loader: {
-//         provide: TranslateLoader,
-//         useFactory: HttpLoaderFactory,
-//         deps: [HttpClient]
-//       }
-//     }).providers
-//   ]
-// };
-
-// import { ApplicationConfig } from '@angular/core';
-// import { provideRouter } from '@angular/router';
-// import { provideClientHydration } from '@angular/platform-browser';
-// import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
-// import { provideHttpClient } from '@angular/common/http';
-// import { TranslateModule, TranslateLoader } from '@ngx-translate/core';
-// import { TranslateHttpLoader } from '@ngx-translate/http-loader';
-// import { HttpClient } from '@angular/common/http';
-// import { routes } from './app.routes';
-
-// // Funktion, um den HttpLoader für die Übersetzungsdateien bereitzustellen
-// export function HttpLoaderFactory(http: HttpClient) {
-//   return new TranslateHttpLoader(http, './assets/i18n/', '.json');
-// }
-
-// export const appConfig: ApplicationConfig = {
-//   providers: [
-//     provideRouter(routes), // Routes einbinden
-//     provideClientHydration(),
-//     provideAnimationsAsync(),
-//     provideHttpClient(),
-//     ...(TranslateModule.forRoot({
-//       loader: {
-//         provide: TranslateLoader,
-//         useFactory: HttpLoaderFactory,
-//         deps: [HttpClient]
-//       }
-//     }).providers ?? [])
-//   ]
-// };
-
-
-import { ApplicationConfig } from '@angular/core';
+import { ApplicationConfig, APP_INITIALIZER } from '@angular/core';
 import { provideRouter } from '@angular/router';
 import { provideClientHydration } from '@angular/platform-browser';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
@@ -215,19 +14,30 @@ export function HttpLoaderFactory(http: HttpClient) {
   return new TranslateHttpLoader(http, './assets/i18n/', '.json');
 }
 
+// Funktion, um die Standardsprache auf Deutsch zu setzen
+export function initializeTranslateService(translate: TranslateService) {
+  return () => translate.use('de');
+}
+
 export const appConfig: ApplicationConfig = {
   providers: [
     provideRouter(routes), // Routes einbinden
     provideClientHydration(),
     provideAnimationsAsync(),
     provideHttpClient(),
-    TranslateService, // Hinzufügen von TranslateService
+    TranslateService,
     ...(TranslateModule.forRoot({
       loader: {
         provide: TranslateLoader,
         useFactory: HttpLoaderFactory,
         deps: [HttpClient]
       }
-    }).providers ?? [])
+    }).providers ?? []),
+    {
+      provide: APP_INITIALIZER,
+      useFactory: initializeTranslateService,
+      deps: [TranslateService],
+      multi: true
+    }
   ]
 };
